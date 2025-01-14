41ac72a8f2709b55fa1dcbe59d538904a3686b19
tobiasGuta <156011774+tobiasGuta@users.noreply.github.com>
2025-01-14 13:40:29 -0500

    Update mytool.sh

--- mytool.sh
+++ mytool.sh
@@ -3,3 +3,7 @@
 # Install Nuclei using Go
 echo "Installing Nuclei..."
 go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+
+# Clone the DirSearch repository
+echo "Cloning DirSearch repository..."
+git clone https://github.com/maurosoria/dirsearch.git --depth 1
